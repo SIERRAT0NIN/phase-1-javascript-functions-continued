8d0276255cb440771470f36afe80f765a36a0066
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-12 17:25:02 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+// function wrapAdjective("*"){
+//   return function(){
+//     const adjective = "highlight"
+//    console.log(adjective);
+//   }
+// }
+// wrapAdjective("*")
+
+const wrapAdjective = (style = "*") => {
+  return function (adjective = "special") {
+    return `You are ${style}${adjective}${style}!`;
+  };
+};
